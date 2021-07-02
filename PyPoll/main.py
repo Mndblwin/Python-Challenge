958babb01bfbafacc429a3e468400bb5b0e62eb9
kerry.olden@gmail.com <kerry.olden@gmail.com>
2021-07-02 17:08:28 -0500

    added headers and winner title

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -25,12 +25,13 @@ for row in csvreader:
     voter_id.append(row[0])
     county.append(row[1])
     candidate.append(row[2])
-#print(len('Total Votes: '+ str(voter_id)))
-# vote_counts = [int(i) for i in voter_id if i != "NA"]
-#vote_counts.count(voter_id)
+
+print(f"Election Results\n"
+    f"--------------------------------")
+
 print(f"Total Votes: {len(voter_id)}")
-#print(f"Khan: {sum(candidate.count(['Khan']))}")
-#print(candidate.count('Khan'))
+
+print(f"--------------------------------")
 candidate_Khan = candidate.count('Khan')
 percent_Khan = candidate.count('Khan') / len(voter_id) * 100
 print(f"Khan: {percent_Khan} {candidate_Khan}")
@@ -46,10 +47,11 @@ print(f"Li: {percent_Li} {candidate_Li}")
 candidate_OTooley = candidate.count("O'Tooley")
 percent_OTooley = candidate.count("O'Tooley") / len(voter_id) * 100
 print(f"OTooley: {percent_OTooley} {candidate_OTooley}")
-    
+
+print(f"--------------------------------")
+
 def winner (candidate):
     return max(set(candidate), key = candidate.count)
-print(winner(candidate))
-
+print(f'Winner:' + str(winner(candidate)))
 
 
