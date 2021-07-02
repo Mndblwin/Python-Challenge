a84d2aa3b676ef85a4f171a2df2233effd4a75b7
kerry.olden@gmail.com <kerry.olden@gmail.com>
2021-07-02 18:00:43 -0500

    updated output file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -34,20 +34,24 @@ print(f"Total Votes: {len(voter_id)}")
 
 print(f"--------------------------------")
 candidate_Khan = candidate.count('Khan')
-percent_Khan = candidate.count('Khan') / len(voter_id) * 100
-print(f"Khan: {percent_Khan} {candidate_Khan}")
+percent_Khan = (candidate.count('Khan') / (len(voter_id)) * 100)
+round_Khan = round(percent_Khan, 2)
+print(f"Khan: {round_Khan}{candidate_Khan}")
 
 candidate_Correy = candidate.count('Correy')
-percent_Correy = candidate.count('Correy') / len(voter_id) * 100
-print(f"Correy: {percent_Correy} {candidate_Correy}")
+percent_Correy = (candidate.count('Correy') / (len(voter_id)) * 100)
+round_Correy = round(percent_Correy, 2)
+print(f"Correy: {round_Correy} {candidate_Correy}")
 
 candidate_Li = candidate.count('Li')
-percent_Li = candidate.count('Li') / len(voter_id) * 100
-print(f"Li: {percent_Li} {candidate_Li}")
+percent_Li = (candidate.count('Li') / (len(voter_id)) * 100)
+round_Li = round(percent_Correy, 2)
+print(f"Li: {round_Li} {candidate_Li}")
 
 candidate_OTooley = candidate.count("O'Tooley")
-percent_OTooley = candidate.count("O'Tooley") / len(voter_id) * 100
-print(f"OTooley: {percent_OTooley} {candidate_OTooley}")
+percent_OTooley = (candidate.count("O'Tooley") / (len(voter_id)) * 100)
+round_OTooley = round(percent_OTooley, 2)
+print(f"OTooley: {round_OTooley} {candidate_OTooley}")
 
 print(f"--------------------------------")
 
@@ -60,4 +64,10 @@ with open(outfile, 'w') as textfile:
     textfile.write(f"Election Results\n"
                 f"--------------------------------\n"
                 f"Total Votes: {len(voter_id)}\n"
+                f"--------------------------------\n")
+
+    textfile.write(f"Khan: {round_Khan} {candidate_Khan}\n"
+                f"Correy: {round_Correy} {candidate_Correy}\n"
+                f"Li: {round_Li} {candidate_Li}\n"
+                f"OTooley: {round_OTooley} {candidate_OTooley}\n"
                 f"--------------------------------\n")
