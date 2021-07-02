05f2f7d12cad70754ebb0e7959a34862e0ef3d55
kerry.olden@gmail.com <kerry.olden@gmail.com>
2021-07-02 13:49:23 -0500

    total election votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,7 @@
 import os
 import csv
 import statistics
+from typing import Counter
 
  # Set path for file
 csvpath = os.path.join("PyPoll", "election_data.csv")
@@ -16,3 +17,17 @@ csvreader = csv.reader(csvfile, delimiter=",")
 voter_id = []
 county = []
 candidate = []
+
+# Skips header
+header = next(csvreader)
+
+for row in csvreader:
+    voter_id.append(row[0])
+#print(len('Total Votes: '+ str(voter_id)))
+# vote_counts = [int(i) for i in voter_id if i != "NA"]
+#vote_counts.count(voter_id)
+print(f"Total Votes: {len(voter_id)}")
+    
+
+
+
